9d041115bd01839f71d80fc8cbd8413c0230db7d
Алексей Ларионов <Alakey060711@yandex.ru>
2023-05-30 22:01:09 +0500

    Validation of data obtained from a web form

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -4,6 +4,8 @@ from .forms import BirthdayForm
 
 
 def birthday(request):
-    form = BirthdayForm()
+    form = BirthdayForm(request.GET or None)
+    if form.is_valid():
+        pass
     context = {'form': form}
     return render(request, 'birthday/birthday.html', context)
