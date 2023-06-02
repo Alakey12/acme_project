1f93c21d8549782bf651f18c24cabec62d2a5c32
Алексей Ларионов <Alakey060711@yandex.ru>
2023-06-02 13:30:19 +0500

    Working with images in forms

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -10,8 +10,15 @@ def birthday(request, pk=None):
         instance = get_object_or_404(Birthday, pk=pk)
     else:
         instance = None
-    form = BirthdayForm(request.POST or None, instance=instance)
+
+    form = BirthdayForm(
+        request.POST or None,
+        files=request.FILES or None,
+        instance=instance
+        )
+
     context = {'form': form}
+
     if form.is_valid():
         form.save()
         birthday_countdown = calculate_birthday_countdown(
@@ -20,7 +27,9 @@ def birthday(request, pk=None):
         context.update(
             {'birthday_countdown': birthday_countdown}
         )
+
     template = 'birthday/birthday.html'
+
     return render(request, template, context)
 
 
